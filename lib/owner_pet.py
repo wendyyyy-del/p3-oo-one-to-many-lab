3d5eca1f99183ee28f26e9c2cabcf2efff79f05b
sarah <sarah.akelo@student.moringaschool.com>
2025-05-22 08:43:57 +0300

    Complete

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,29 @@
 class Pet:
-    pass
+    PET_TYPES= ['dog', 'cat', 'rodent', 'bird', 'reptile', 'exotic']
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception ("Invalid pet type.")
+
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+           raise Exception("Can only add instances of Pet.")
+        if pet.pet_type not in Pet.PET_TYPES:
+            raise Exception("Invalid pet type.")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
